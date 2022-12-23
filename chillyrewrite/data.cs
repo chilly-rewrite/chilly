57db57fd86dc916ba31179f660855558baa8a019
x4caa <jaxbell37@gmail.com>
2022-12-22 20:27:00 -0800

    Add project files.

---
+++ chillyrewrite/data.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace chillyrewrite
+{
+    public class Entity
+    {
+        public string type;
+        public Vector3 head, feet, velocity;
+        public Vector2 hitbox;
+    }
+}
