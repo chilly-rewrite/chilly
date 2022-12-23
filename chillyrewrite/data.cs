a43fc384d02f2e83ae4ec4ef6939b3dd1371f42b
x4caa <jaxbell37@gmail.com>
2022-12-23 12:02:49 -0800

    Revert "Revert "Add project files.""
    This reverts commit 6c0c8feb3cdadd149c4e3d7b7600bb96ce299c90.

---
+++ chillyrewrite/data.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+namespace chillyrewrite
+{
+    public class Entity
+    {
+        public string type;
+        public Vector3 head, feet, velocity;
+        public Vector2 hitbox;
+        public int baseAddress;
+    }
+}
---
+++ chillyrewrite/offsets.cs
@@ -0,0 +1,20 @@
+﻿namespace chillyrewrite
+{
+    public class offset
+    {
+        public static string
+            iEntityList = "Minecraft.Windows.exe+0498256804982568,0,18,B8,8,C8,810,48,0,0",
+            iLocalPlayer = "Minecraft.Windows.exe+0498256804982568,0,18,B8,8,C8,810,48,0,0,0",
+
+
+            //offsets from entity class
+            sType = ",320,0",
+            v3Head = "358,0",
+            v3Feet = "358,C",
+            v3Velocity = "350,18",
+            v2Hitbox = "358,18";
+
+
+
+    }
+}
